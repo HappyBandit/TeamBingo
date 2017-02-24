87414f3d9ace7004358d952db14ac493c6d8e2f0
HauptmannEck <eckhoffj@gmail.com>
2017-02-24 16:28:06 -0600

    more update deploy6

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-chmod 600 id_rsa
-mv id_rsa ~/.ssh/id_rsa
-scp -r -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no build $SERVURL
+chmod 600 ~/.ssh/id_rsa
+scp -r -i ~/.ssh/id_rsa build $SERVURL
