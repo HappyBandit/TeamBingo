5a3e4f911c31c558cc2c09f3e5f1dcff05ef333d
HauptmannEck <eckhoffj@gmail.com>
2017-02-24 11:49:20 -0600

    more update deploy2

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 chmod 600 id_rsa
 mv id_rsa ~/.ssh/id_rsa
-scp -r build $SERVURL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
+scp -r build $SERVURL -o StrictHostKeyChecking=no
